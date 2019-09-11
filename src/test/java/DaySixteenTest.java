04447f12791e86b7cfe5a10555b947b1c37ac7f4
BenjaminNews <benjamin.kane@news.co.uk>
2019-09-11 17:30:53 +0100

    addig day sixteen

--- src/test/java/DaySixteenTest.java
+++ src/test/java/DaySixteenTest.java
@@ -1,5 +1,12 @@
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class DaySixteenTest {
 
+    @Test
+    public void givenInput_return_baedc() {
+        DaySixteen daySixteen = new DaySixteen();
+        assertEquals("baedc", daySixteen.getResult("s1,x3/4,pe/b", "abcde", 1));
+    }
 }
