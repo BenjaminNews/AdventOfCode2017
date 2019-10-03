c5271f3bcdddbcb9baa41b48ae2c660e958b9239
BenjaminNews <benjamin.kane@news.co.uk>
2019-10-03 09:40:14 +0100

    adding day twelve

--- src/test/java/DayTwelveTest.java
+++ src/test/java/DayTwelveTest.java
@@ -1,5 +1,36 @@
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class DayTwelveTest {
 
+    @Test
+    public void givenInput_returnPipeOfLength_6() {
+        DayTwelve dayTwelve = new DayTwelve();
+        String input =  "0 <-> 2\n" +
+                        "1 <-> 1\n" +
+                        "2 <-> 0, 3, 4\n" +
+                        "3 <-> 2, 4\n" +
+                        "4 <-> 2, 3, 6\n" +
+                        "5 <-> 6\n" +
+                        "6 <-> 4, 5";
+
+        dayTwelve.setup(input);
+        assertEquals(6, dayTwelve.getPipeLength(0));
+    }
+
+    @Test
+    public void givenInput_aGroupCountOf_two() {
+        DayTwelve dayTwelve = new DayTwelve();
+        String input =  "0 <-> 2\n" +
+                        "1 <-> 1\n" +
+                        "2 <-> 0, 3, 4\n" +
+                        "3 <-> 2, 4\n" +
+                        "4 <-> 2, 3, 6\n" +
+                        "5 <-> 6\n" +
+                        "6 <-> 4, 5";
+
+        dayTwelve.setup(input);
+        assertEquals(2, dayTwelve.getGroups());
+    }
 }
