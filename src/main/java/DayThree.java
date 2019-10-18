1d3ec7d30a243848bdd48e3e7a0e3b636f29f73e
BenjaminNews <benjamin.kane@news.co.uk>
2019-10-18 16:42:06 +0100

    adding day three

--- src/main/java/DayThree.java
+++ src/main/java/DayThree.java
@@ -1,2 +1,240 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class DayThree {
+
+    private static long[][] spiral;
+
+    public static int partOne(int size, int endPosition) {
+        setupPartOne(size);
+        return getDistanceFromStartPosition(endPosition);
+    }
+
+    private static int getDistanceFromStartPosition(int endPosition) {
+        int[] end_X_Y = getX_Y_coordinates(endPosition);
+        int[] start_X_Y = getX_Y_coordinates(1);
+        return Math.abs(start_X_Y[0] - end_X_Y[0]) + Math.abs(start_X_Y[1] - end_X_Y[1]);
+    }
+
+    private static void setupPartOne(int size) {
+        spiral = new long[size + 1][size + 1];
+        int xCor = (int) Math.floor(size / 2);
+        int yCor = (int) Math.floor(size / 2);
+
+        if(xCor % 2 == 1)
+            xCor++;
+        if (yCor % 2 == 1)
+            yCor++;
+        boolean NORTH = false;
+        boolean EAST  = true;
+        boolean WEST  = false;
+        int value = 1;
+
+        for(int i = 0; i < (size * size); i++) {
+            if(i == 0) {
+                spiral[xCor][yCor] = value++;
+                yCor++;
+            } else {
+                if(EAST) {
+                    spiral[xCor][yCor] = value++;
+                    if(spiral[xCor - 1][yCor] == 0) {
+                        EAST = false; NORTH = true;
+                        xCor--;
+                    } else {
+                        yCor++;
+                    }
+                } else if(NORTH) {
+                    spiral[xCor][yCor] = value++;
+                    if(spiral[xCor][yCor - 1] == 0) {
+                        WEST = true; NORTH = false;
+                        yCor--;
+                    } else {
+                        xCor--;
+                    }
+                } else if(WEST) {
+                    spiral[xCor][yCor] = value++;
+                    if(spiral[xCor + 1][yCor] == 0) {
+                        xCor++;
+                        WEST = false;
+                    } else {
+                        yCor--;
+                    }
+                } else {
+                    spiral[xCor][yCor] = value++;
+                    if(spiral[xCor][yCor + 1] == 0) {
+                        yCor++;
+                        EAST = true;
+                    } else {
+                        xCor++;
+                    }
+                }
+            }
+        }
+    }
+
+    public static long setupPartTwo(int size, int endPosition) {
+        spiral = new long[size + 1][size + 1];
+        int xCor = (int) Math.floor(size / 2);
+        int yCor = (int) Math.floor(size / 2);
+
+        if(xCor % 2 == 1)
+            xCor++;
+        if (yCor % 2 == 1)
+            yCor++;
+        boolean NORTH = false;
+        boolean EAST  = true;
+        boolean WEST  = false;
+
+        boolean FOUND = false;
+        int i = 0;
+        while(!FOUND) {
+            if(!FOUND) {
+                if (i == 0) {
+                    spiral[xCor][yCor] = 1;
+                    yCor++;
+                } else {
+                    if (EAST) {
+                        long temp = getSurroundingValue(xCor, yCor);
+                        spiral[xCor][yCor] = temp;
+                        if (spiral[xCor - 1][yCor] == 0) {
+                            EAST = false;
+                            NORTH = true;
+                            xCor--;
+                        } else {
+                            yCor++;
+                        }
+                        if (temp > endPosition) {
+                            return temp;
+                        }
+                    } else if (NORTH) {
+                        long temp = getSurroundingValue(xCor, yCor);
+                        spiral[xCor][yCor] = temp;
+
+                        if (spiral[xCor][yCor - 1] == 0) {
+                            WEST = true;
+                            NORTH = false;
+                            yCor--;
+                        } else {
+                            xCor--;
+                        }
+                        if (temp > endPosition) {
+                            return temp;
+                        }
+                    } else if (WEST) {
+                        long temp = getSurroundingValue(xCor, yCor);
+                        spiral[xCor][yCor] = temp;
+                        if (spiral[xCor + 1][yCor] == 0) {
+                            xCor++;
+                            WEST = false;
+                        } else {
+                            yCor--;
+                        }
+                        if (temp > endPosition) {
+                            return temp;
+                        }
+                    } else {
+                        long temp = getSurroundingValue(xCor, yCor);
+                        spiral[xCor][yCor] = temp;
+                        if (spiral[xCor][yCor + 1] == 0) {
+                            yCor++;
+                            EAST = true;
+                        } else {
+                            xCor++;
+                        }
+                        if (temp > endPosition) {
+                            return temp;
+                        }
+                    }
+                }
+                i++;
+            } else {
+                break;
+            }
+        }
+        return getSurroundingValue(xCor, yCor);
+    }
+
+    private static long getSurroundingValue(int xCor, int yCor) throws ArrayIndexOutOfBoundsException {
+        long northValue = 0;
+        try {
+            northValue = spiral[xCor - 1] [yCor];
+        } catch (ArrayIndexOutOfBoundsException ignored) { }
+
+        long eastValue = 0;
+        try {
+            eastValue = spiral[xCor][yCor + 1];
+        } catch (ArrayIndexOutOfBoundsException ignored) { }
+
+        long southValue = 0;
+        try {
+            southValue = spiral[xCor + 1][yCor];
+        }  catch (ArrayIndexOutOfBoundsException ignored) { }
+
+        long westValue = 0;
+        try {
+            westValue = spiral[xCor][yCor - 1];
+        } catch (ArrayIndexOutOfBoundsException ignored) { }
+
+        long northWestValue = 0;
+        try {
+            northWestValue = spiral[xCor - 1][yCor - 1];
+        } catch (ArrayIndexOutOfBoundsException ignored) { }
+
+        long northEastValue = 0;
+        try {
+            northEastValue = spiral[xCor - 1][ yCor + 1];
+        } catch (ArrayIndexOutOfBoundsException ignored) { }
+
+        long southWestValue = 0;
+        southWestValue = spiral[xCor + 1][yCor - 1];
+
+        long southEastValue = 0;
+        try {
+            southEastValue = spiral[xCor + 1][yCor + 1];
+        } catch (ArrayIndexOutOfBoundsException ignored) { }
+
+        return  northValue +
+                eastValue  +
+                southValue +
+                westValue  +
+                northWestValue +
+                northEastValue +
+                southWestValue +
+                southEastValue;
+    }
+
+    private static void print() {
+        for (long[] arr : spiral) {
+            StringBuilder segment = new StringBuilder();
+            for (long i : arr) {
+                if (i < 10) {
+                    segment.append("   ").append(i);
+                } else if (i < 100) {
+                    segment.append("  ").append(i);
+                } else if (i < 1000) {
+                    segment.append(" ").append(i);
+                }
+            }
+            System.out.println(segment);
+        }
+    }
+
+    private static int[] getX_Y_coordinates(int position) {
+        int[] x_y = new int[2];
+        for (int x = 0; x < spiral.length; ++x) {
+            for (int y = 0; y < spiral.length; ++y) {
+                if (spiral[x][y] == position ) {
+                    x_y[0] = x;
+                    x_y[1] = y;
+                    return x_y;
+                }
+            }
+        }
+        return x_y;
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("Part one: %d%n", DayThree.partOne(312051 / 100, 312051));
+        System.out.printf("Part two: %d%n", DayThree.setupPartTwo(312051 / 100, 312051));
+    }
 }
--- src/test/java/DayThreeTest.java
+++ src/test/java/DayThreeTest.java
@@ -1,5 +1,130 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class DayThreeTest {
 
+    @Test
+    public void givenValueOf_1_packetIs_1_stepsAway() {
+        assertEquals(0, DayThree.partOne(1, 1));
+    }
+
+    @Test
+    public void givenValueOf_12_packetIs_3_stepsAway() {
+        assertEquals(3, DayThree.partOne(12, 12));
+    }
+
+    @Test
+    public void givenValueOf_23_packetIs_2_stepsAway() {
+        assertEquals(2, DayThree.partOne(23, 23));
+    }
+
+    @Test
+    public void givenValueOf_1024_packetIs_31_stepsAway() {
+        assertEquals(31, DayThree.partOne(1024, 1024));
+    }
+
+    @Test
+    public void givenValueOf_1_firstLargerPacketIs_2() {
+        assertEquals(2, DayThree.setupPartTwo(255, 1));
+    }
+
+    @Test
+    public void givenValueOf_2_firstLargerPacketIs_4() {
+        assertEquals(4, DayThree.setupPartTwo(255, 2));
+    }
+
+    @Test
+    public void givenValueOf_4_firstLargerPacketIs_5() {
+        assertEquals(5, DayThree.setupPartTwo(255, 4));
+    }
+
+    @Test
+    public void givenValueOf_5_firstLargerPacketIs_10() {
+        assertEquals(10, DayThree.setupPartTwo(255, 5));
+    }
+
+    @Test
+    public void givenValueOf_10_firstLargerPacketIs_11() {
+        assertEquals(11, DayThree.setupPartTwo(255, 10));
+    }
+
+    @Test
+    public void givenValueOf_11_firstLargerPacketIs_23() {
+        assertEquals(23, DayThree.setupPartTwo(255, 11));
+    }
+
+    @Test
+    public void givenValueOf_23_firstLargerPacketIs_25() {
+        assertEquals(25, DayThree.setupPartTwo(255, 23));
+    }
+
+    @Test
+    public void givenValueOf_25_firstLargerPacketIs_26() {
+        assertEquals(26, DayThree.setupPartTwo(255, 25));
+    }
+
+    @Test
+    public void givenValueOf_26_firstLargerPacketIs_54() {
+        assertEquals(54, DayThree.setupPartTwo(255, 26));
+    }
+
+    @Test
+    public void givenValueOf_54_firstLargerPacketIs_57() {
+        assertEquals(57, DayThree.setupPartTwo(255, 54));
+    }
+
+    @Test
+    public void givenValueOf_57_firstLargerPacketIs_59() {
+        assertEquals(59, DayThree.setupPartTwo(255, 57));
+    }
+
+    @Test
+    public void givenValueOf_59_firstLargerPacketIs_122() {
+        assertEquals(122, DayThree.setupPartTwo(255, 59));
+    }
+
+    @Test
+    public void givenValueOf_122_firstLargerPacketIs_133() {
+        assertEquals(133, DayThree.setupPartTwo(255, 122));
+    }
+
+    @Test
+    public void givenValueOf_133_firstLargerPacketIs_142() {
+        assertEquals(142, DayThree.setupPartTwo(255, 133));
+    }
+
+    @Test
+    public void givenValueOf_142_firstLargerPacketIs_147() {
+        assertEquals(147, DayThree.setupPartTwo(255, 142));
+    }
+
+    @Test
+    public void givenValueOf_147_firstLargerPacketIs_304() {
+        assertEquals(304, DayThree.setupPartTwo(255, 147));
+    }
+
+    @Test
+    public void givenValueOf_304_firstLargerPacketIs_330() {
+        assertEquals(330, DayThree.setupPartTwo(255, 304));
+    }
+
+    @Test
+    public void givenValueOf_330_firstLargerPacketIs_351() {
+        assertEquals(351, DayThree.setupPartTwo(255, 330));
+    }
+
+    @Test
+    public void givenValueOf_351_firstLargerPacketIs_362() {
+        assertEquals(362, DayThree.setupPartTwo(255, 351));
+    }
+
+    @Test
+    public void givenValueOf_362_firstLargerPacketIs_747() {
+        assertEquals(747, DayThree.setupPartTwo(255, 362));
+    }
+
+    @Test
+    public void givenValueOf_747_firstLargerPacketIs_806() {
+        assertEquals(806, DayThree.setupPartTwo(255, 747));
+    }
 }
