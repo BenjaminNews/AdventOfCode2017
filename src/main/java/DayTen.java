89a34867fc1dfa2263229a61ae17ea63f392e5a2
BenjaminNews <benjamin.kane@news.co.uk>
2019-11-02 16:28:43 +0000

    adding day ten part two and refactorered

--- src/main/java/DayTen.java
+++ src/main/java/DayTen.java
@@ -1,88 +1,62 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DayTen {
-
-    private int[] memoryValues;
-    private int[] flipValues;
-    private int[] reverseArray;
-
-    private int index = 0;
+    private int[] list;
+    private int pos = 0;
     private int skipSize = 0;
 
-    public DayTen (int memoryValues, String flipValues, String splitterator) {
-        this.memoryValues = IntStream.range(0, memoryValues).toArray();
-        this.flipValues = Arrays.stream(flipValues.split(splitterator)).mapToInt(Integer::parseInt).toArray();
+    public int twist(int setSize, String input, String splitterator) {
+        List<Integer> lengths = Arrays.stream(input.split(splitterator)).map(Integer::parseInt).collect(Collectors.toList());
+        list = IntStream.iterate(0, n -> n + 1).limit(setSize).toArray();
+        twistSubSet(lengths);
+        return list[0] * list[1];
     }
 
-    public int twist() {
-        for(int flipValue : flipValues) {
-            if (index + flipValue < memoryValues.length) {
-                reverseArrayWhichExistsWithoutWrap(flipValue);
-            } else {
-                reverseArrayWhichWraps(flipValue);
-            }
-
-            index = index + flipValue + skipSize++;
-
-            if (index > memoryValues.length) {
-                index = index - memoryValues.length;
-            }
-        }
-        return memoryValues[0] * memoryValues[1];
-    }
-
-    private void reverseArrayWhichExistsWithoutWrap(int flipValue) {
-        reverseArray = Arrays.copyOfRange(memoryValues, index, index + flipValue);
-        reverseTheArray();
-        updateValues();
-    }
-
-    private void reverseArrayWhichWraps(int flipValue) {
-        int endPositionOfWrap = (index + flipValue) - memoryValues.length;
-        getWrappedArrayContents(flipValue, endPositionOfWrap);
-        reverseTheArray();
-        updateValues();
-    }
-
-    private void getWrappedArrayContents(int flipValue, int endPositionOfWrap) {
-        int[] startOfArrayToTwist = Arrays.copyOfRange(memoryValues, index, memoryValues.length);
-        int[] endOfArrayToTwist = Arrays.copyOfRange(memoryValues, 0, endPositionOfWrap);
-        reverseArray = new int[flipValue];
-        System.arraycopy(startOfArrayToTwist, 0, reverseArray, 0, startOfArrayToTwist.length);
-        System.arraycopy(endOfArrayToTwist, 0, reverseArray, startOfArrayToTwist.length, endOfArrayToTwist.length);
-    }
-
-    private void reverseTheArray() {
-        int arrayLength = reverseArray.length;
-        for (int i = 0; i < arrayLength / 2; i++) {
-            int temp = reverseArray[i];
-            reverseArray[i] = reverseArray[arrayLength - 1 - i];
-            reverseArray[arrayLength - 1 - i] = temp;
+    private void twistSubSet(List<Integer> lengths) {
+        for (int length : lengths) {
+            spin(length);
+            pos += length + skipSize;
+            skipSize++;
         }
     }
 
-    private void updateValues() {
-        for(int insertPoint = index, arrayToInsertFromIndex = 0; arrayToInsertFromIndex < reverseArray.length; insertPoint++, arrayToInsertFromIndex++) {
-            memoryValues[insertPoint] = reverseArray[arrayToInsertFromIndex];
-            if(insertPoint + 1 == memoryValues.length) {
-                insertPoint = -1;
-            }
+    public String twistPartTwo(int setSize, String input, int[] additionalLengths) {
+        List<Integer> lengths = input.chars().boxed().collect(Collectors.toList());
+        Collections.addAll(lengths, Arrays.stream(additionalLengths).boxed().toArray(Integer[]::new));
+        list = IntStream.iterate(0, n -> n + 1).limit(setSize).toArray();
+        for(int x = 0; x < 64; x++) {
+            twistSubSet(lengths);
         }
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < list.length; i += 16) {
+            int XORList = list[i] ^ list[i + 1];
+            for(int x = i + 2; x < i + 16; x++) {
+                XORList ^= list[x];
+            }
+            result.append(Integer.toHexString(XORList).length() == 1 ? "0" + Integer.toHexString(XORList) : Integer.toHexString(XORList));
+        }
+        return result.toString();
     }
 
-    public int[] memoryValues() {
-        return memoryValues;
-    }
-
-    public int getIndex() {
-        return this.index;
+    private void spin(int length) {
+        for (int i = 0; i < length / 2; i++) {
+            int start = (pos + i) % list.length;
+            int end = (pos + length - 1 - i) % list.length;
+            int tmp = list[start];
+            list[start] = list[end];
+            list[end] = tmp;
+        }
     }
 
     public static void main(String[] args) {
-        DayTen dayTen = new DayTen(256, input, ",");
-        int result = dayTen.twist();
-        System.out.printf("Part one: %d", result);
+        DayTen dayTen = new DayTen();
+        System.out.println(dayTen.twist(256, input, ","));
+        dayTen = new DayTen();
+        System.out.println(dayTen.twistPartTwo(256, input, new int[] { 17, 31, 73, 47, 23 }));
     }
 
     private static final String input = "34,88,2,222,254,93,150,0,199,255,39,32,137,136,1,167";
--- src/test/java/DayTenTest.java
+++ src/test/java/DayTenTest.java
@@ -1,38 +1,40 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.omg.CORBA.DATA_CONVERSION;
 
 public class DayTenTest {
 
-    @Test
-    public void givenAnArrayWithValues_0_1_2_3_4_andAnIndexOf_0_andATwistSizeOf_3_return_2_1_0_3_4() {
-        DayTen dayTen = new DayTen(5, "3, ", ", ");
-        dayTen.twist();
-        assertArrayEquals(new int[] {2, 1, 0, 3, 4}, dayTen.memoryValues());
-        assertEquals(3, dayTen.getIndex());
+    private DayTen dayTen;
+
+    @Before
+    public void setup() {
+        this.dayTen = new DayTen();
     }
 
     @Test
-    public void givenAnArrayWithValues_0_1_2_3_4_anIndexOf_0_andATwistSizeOf_3_4_return_4_3_0_1_2() {
-        DayTen dayTen = new DayTen(5, "3, 4, ", ", ");
-        dayTen.twist();
-        assertArrayEquals(new int[] {4, 3, 0, 1, 2}, dayTen.memoryValues());
-        assertEquals(3, dayTen.getIndex());
+    public void givenAnArrayWithValues_0_1_2_3_4_andAnIndexOf_0_andATwistSizeOf_3_returnScoreOf_2() {
+        assertEquals(12,  dayTen.twist(5, "3, 4, 1, 5", ", "));
     }
 
     @Test
-    public void givenAnArrayWithValues_0_1_2_3_4_anIndexOf_0_andATwistSizeOf_3_4_1_return_4_3_0_1_2() {
-        DayTen dayTen = new DayTen(5, "3, 4, 1, ", ", ");
-        dayTen.twist();
-        assertArrayEquals(new int[] {4, 3, 0, 1, 2}, dayTen.memoryValues());
-        assertEquals(1, dayTen.getIndex());
+    public void givenEmptyInput_returns_a2582a3a0e66e6e86e3812dcb672a272() {
+        assertEquals("a2582a3a0e66e6e86e3812dcb672a272", dayTen.twistPartTwo(256, "", new int[] { 17, 31, 73, 47, 23 }));
     }
 
     @Test
-    public void givenAnArrayWithValues_0_1_2_3_4_anIndexOf_0_andATwistSizeOf_3_4_1_5_return_3_4_2_1_0_withScoreOf_12() {
-        DayTen dayTen = new DayTen(5, "3, 4, 1, 5, ", ", ");
-        assertEquals(12, dayTen.twist());
-        assertArrayEquals(new int[] {3, 4, 2, 1, 0}, dayTen.memoryValues());
-        assertEquals(4, dayTen.getIndex());
+    public void givenInput_AoC_2017_returns_33efeb34ea91902bb2f59c9920caa6cd() {
+        assertEquals("33efeb34ea91902bb2f59c9920caa6cd", dayTen.twistPartTwo(256, "AoC 2017", new int[] { 17, 31, 73, 47, 23 }));
+    }
+
+    @Test
+    public void givenInput_1_2_3_returns_3efbe78a8d82f29979031a4aa0b16a9d() {
+        assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", dayTen.twistPartTwo(256, "1,2,3", new int[] { 17, 31, 73, 47, 23 }));
+    }
+
+    @Test
+    public void givenInput_1_2_4_returns_63960835bcdc130f0b66d7ff4f6a5a8e() {
+        assertEquals("63960835bcdc130f0b66d7ff4f6a5a8e", dayTen.twistPartTwo(256, "1,2,4", new int[] { 17, 31, 73, 47, 23 }));
     }
 }
